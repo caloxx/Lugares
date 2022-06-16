38e96f2a146afccf3538532eca423a82d7cd41d2
Andres Navarrete <andresnboza92@gmail.com>
2022-06-15 19:58:43 -0600

    delete completed

--- app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,10 +1,9 @@
 package com.example.lugares.ui.lugar
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -45,7 +44,21 @@ class UpdateLugarFragment : Fragment() {
 
         binding.finalAddLugarBtn.setOnClickListener { updateLugar() }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // se es el borrado
+        if (item.itemId == R.id.menu_delete) {
+            deleteLugar()
+        }
+        return  super.onOptionsItemSelected(item)
     }
 
     private fun updateLugar() {
@@ -60,6 +73,21 @@ class UpdateLugarFragment : Fragment() {
         } else {
             Toast.makeText(requireContext(), getString(R.string.msg_error), Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun deleteLugar(){
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setPositiveButton(getString(R.string.si)) { _,_ ->
+            lugarViewModel.deleteLugar(args.lugar)
+            Toast.makeText(requireContext(), getString(R.string.deleted) + " ${args.lugar.nombre}", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_updateLugarFragment_to_nav_lugar)
+        }
+
+        builder.setNegativeButton(getString(R.string.no)) { _,_ -> }
+        builder.setTitle(R.string.deleted)
+        builder.setMessage(getString(R.string.seguroBorrar) + " ${args.lugar.nombre}?")
+        builder.create().show()
     }
 
     private fun validation (nombre: String): Boolean {
